904786919ba06e4a47488b2da744818d77446209
Brianna Keune <briannakeune@gmail.com>
2020-01-04 16:29:33 -0600

    updated docs

--- docs/api_project.js
+++ docs/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-01-04T22:23:37.867Z",
+    "time": "2020-01-04T22:29:22.911Z",
     "url": "http://apidocjs.com",
     "version": "0.19.1"
   }
