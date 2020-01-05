68a6158f2461f687d4bf4c51b512bae7373aaa3d
Brianna Keune <briannakeune@gmail.com>
2020-01-04 19:17:13 -0600

    users migration

---
+++ database/migrations/20200104191245_users.js
@@ -0,0 +1,20 @@
+exports.up = function(knex) {
+  return knex.schema.createTable("users", tbl => {
+    tbl.increments();
+    tbl
+      .string("username", 128)
+      .unique()
+      .notNullable();
+    tbl
+      .string("password")
+      .unique()
+      .notNullable();
+    tbl.string("email").unique();
+    tbl.string("name");
+    tbl.integer("is_chef").defaultTo(0);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists("users");
+};
