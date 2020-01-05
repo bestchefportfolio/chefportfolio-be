1472c2c7663bdb652df67664d8e5abcd845379b0
Brianna Keune <briannakeune@gmail.com>
2020-01-04 19:32:34 -0600

    added recipe_ingredients migration

--- database/migrations/20200104191433_recipe_ingredients.js
+++ database/migrations/20200104191433_recipe_ingredients.js
@@ -1,8 +1,34 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("recipe_ingredients", tbl => {
+    tbl.increments();
+    tbl
+      .integer("recipe_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("recipes")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    tbl
+      .integer("quanity_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("quantites")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    tbl
+      .integer("ingredient_id")
+      .unsigned()
+      .notNullable()
+      .references("id")
+      .inTable("ingredients")
+      .onDelete("CASCADE")
+      .onUpdate("CASCADE");
+    tbl.integer("quantity_value").notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists();
 };
