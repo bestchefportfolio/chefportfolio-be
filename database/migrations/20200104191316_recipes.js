e2ac0977a596b048345254985dfa753866de7a5a
Brianna Keune <briannakeune@gmail.com>
2020-01-04 19:18:31 -0600

    added recipes migration

--- database/migrations/20200104191316_recipes.js
+++ database/migrations/20200104191316_recipes.js
@@ -1,8 +1,17 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("recipes", tbl => {
+    tbl.increments();
+    tbl
+      .string("title")
+      .unique()
+      .notNullable();
+    tbl
+      .text("instructions")
+      .unique()
+      .notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("recipes");
 };
