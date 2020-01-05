de5b2e36ea016c9340473858e47d2b0985983a64
Brianna Keune <briannakeune@gmail.com>
2020-01-05 00:22:25 -0600

    added get recipes docs

--- api/routes/chef_recipes/router.js
+++ api/routes/chef_recipes/router.js
@@ -7,27 +7,27 @@ const { addRecipe, getChefById, getChefsRecipes } = require("./model.js");
  * @apiName Add a Recipe
  * @apiGroup Chef Recipes
  *
- * @apiParam {String} title *Required* *Unique* title of recipe.
+ * @apiParam {String} title *Required* **Unique** title of recipe.
  * @apiParam {number} servings *Required* Number of people recipe serves.
  * @apiParam {String} instructions *Required* How to make the recipe.
  *
  * @apiSuccessExample Success-Response:
  *    HTTP/1.1 201 Created
  *    {
- *      chef": {
- *          "chef_name": "New Chef",
- *          "business_name": "Resturant Passport!"
- *       }
+ *      "chef": {
+ *        "chef_name": "Gordan Ramsy",
+ *        "business_name": "Lucky Cat"
+ *      },
  *      "recipes": [
  *        {
  *          "chef_id": 2,
- *          "recipe_id": 9,
- *          "title": "Best Recipe",
- *          "servings": 100,
- *          "instructions": "As many instructions as you want."
+ *          "recipe_id": 4,
+ *          "title": "Stuffed Pork Tenderloin Recipe",
+ *          "servings": 4,
+ *          "instructions": "Preheat the oven to 220°C/Gas 7..."
  *        }
- *      }
- *    }
+ *    ]
+}
  */
 router.post("/:chef_id/recipes/", (req, res) => {
   const chefID = req.params.chef_id;
@@ -42,6 +42,47 @@ router.post("/:chef_id/recipes/", (req, res) => {
       })
       .catch(err => res.status(500).json({ error: err.message }));
   });
+});
+
+/**
+ * @api {post} chef/:chef_id/recipes Add a Recipe
+ * @apiName Add a Recipe
+ * @apiGroup Chef Recipes
+ *
+ * @apiParam {String} title *Required* **Unique** title of recipe.
+ * @apiParam {number} servings *Required* Number of people recipe serves.
+ * @apiParam {String} instructions *Required* How to make the recipe.
+ *
+ * @apiSuccessExample Success-Response:
+ *    HTTP/1.1 201 Created
+ *    {
+ *      "chef": {
+ *        "chef_name": "Gordan Ramsy",
+ *        "business_name": "Lucky Cat"
+ *      },
+ *      "recipes": [
+ *        {
+ *          "chef_id": 2,
+ *          "recipe_id": 4,
+ *          "title": "Stuffed Pork Tenderloin Recipe",
+ *          "servings": 4,
+ *          "instructions": "Preheat the oven to 220°C/Gas 7..."
+ *        },
+ *        {
+ *          "chef_id": 2,
+ *          "recipe_id": 5,
+ *          "title": "Gordon's Breakfast Pizza from Scrambled",
+ *          "servings": 2,
+ *          "instructions": "Remove the pizza dough ball from the fridge and let it warm to room temperature..."
+ *        }     
+ * ]
+}
+ */
+
+router.get("/:chef_id/recipes", (req, res) => {
+  getChefsRecipes(req.params.chef_id)
+    .then(recipes => res.status(200).json({ recipes }))
+    .catch(err => res.status(500).json({ error: err.message }));
 });
 
 module.exports = router;
--- docs/api_data.js
+++ docs/api_data.js
@@ -246,7 +246,7 @@ define({ "api": [
             "type": "String",
             "optional": false,
             "field": "title",
-            "description": "<p><em>Required</em> <em>Unique</em> title of recipe.</p>"
+            "description": "<p><em>Required</em> <strong>Unique</strong> title of recipe.</p>"
           },
           {
             "group": "Parameter",
@@ -269,7 +269,53 @@ define({ "api": [
       "examples": [
         {
           "title": "Success-Response:",
-          "content": "HTTP/1.1 201 Created\n{\n  chef\": {\n      \"chef_name\": \"New Chef\",\n      \"business_name\": \"Resturant Passport!\"\n   }\n  \"recipes\": [\n    {\n      \"chef_id\": 2,\n      \"recipe_id\": 9,\n      \"title\": \"Best Recipe\",\n      \"servings\": 100,\n      \"instructions\": \"As many instructions as you want.\"\n    }\n  }\n}",
+          "content": "   HTTP/1.1 201 Created\n   {\n     \"chef\": {\n       \"chef_name\": \"Gordan Ramsy\",\n       \"business_name\": \"Lucky Cat\"\n     },\n     \"recipes\": [\n       {\n         \"chef_id\": 2,\n         \"recipe_id\": 4,\n         \"title\": \"Stuffed Pork Tenderloin Recipe\",\n         \"servings\": 4,\n         \"instructions\": \"Preheat the oven to 220°C/Gas 7...\"\n       }\n   ]\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "api/routes/chef_recipes/router.js",
+    "groupTitle": "Chef_Recipes"
+  },
+  {
+    "type": "post",
+    "url": "chef/:chef_id/recipes",
+    "title": "Add a Recipe",
+    "name": "Add_a_Recipe",
+    "group": "Chef_Recipes",
+    "parameter": {
+      "fields": {
+        "Parameter": [
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "title",
+            "description": "<p><em>Required</em> <strong>Unique</strong> title of recipe.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "number",
+            "optional": false,
+            "field": "servings",
+            "description": "<p><em>Required</em> Number of people recipe serves.</p>"
+          },
+          {
+            "group": "Parameter",
+            "type": "String",
+            "optional": false,
+            "field": "instructions",
+            "description": "<p><em>Required</em> How to make the recipe.</p>"
+          }
+        ]
+      }
+    },
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "   HTTP/1.1 201 Created\n   {\n     \"chef\": {\n       \"chef_name\": \"Gordan Ramsy\",\n       \"business_name\": \"Lucky Cat\"\n     },\n     \"recipes\": [\n       {\n         \"chef_id\": 2,\n         \"recipe_id\": 4,\n         \"title\": \"Stuffed Pork Tenderloin Recipe\",\n         \"servings\": 4,\n         \"instructions\": \"Preheat the oven to 220°C/Gas 7...\"\n       },\n       {\n         \"chef_id\": 2,\n         \"recipe_id\": 5,\n         \"title\": \"Gordon's Breakfast Pizza from Scrambled\",\n         \"servings\": 2,\n         \"instructions\": \"Remove the pizza dough ball from the fridge and let it warm to room temperature...\"\n       }     \n]\n}",
           "type": "json"
         }
       ]
