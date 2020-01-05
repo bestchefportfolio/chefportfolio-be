3f7859d08ebe9b4270276548e89d10bb8663aaf3
Brianna Keune <briannakeune@gmail.com>
2020-01-04 19:23:37 -0600

    added chef migration

--- database/migrations/20200104191339_quantites.js
+++ database/migrations/20200104191339_quantites.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("quantites", tbl => {
+    tbl.increments();
+    tbl
+      .string("name")
+      .unique()
+      .notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("quantites");
 };
--- database/migrations/20200104191403_chefs.js
+++ database/migrations/20200104191403_chefs.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("chefs", tbl => {
+    tbl.increments();
+    tbl.string("location").notNullable();
+    tbl
+      .string("phone_number")
+      .unique()
+      .notNullable();
+    tbl.string("business_name").unique();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("chefs");
 };
