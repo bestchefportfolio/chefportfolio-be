e5e01b7cb9215237cb7961079ea6b6b2599bf675
Brianna Keune <briannakeune@gmail.com>
2020-01-04 19:19:40 -0600

    added meal_types migration

--- database/migrations/20200104191322_meal_types.js
+++ database/migrations/20200104191322_meal_types.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable("meal_types", tbl => {
+    tbl.increments();
+    tbl
+      .string("type", 128)
+      .unique()
+      .notNullable();
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists("meal_types");
 };
