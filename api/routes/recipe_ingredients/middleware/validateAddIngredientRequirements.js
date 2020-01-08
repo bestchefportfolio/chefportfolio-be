2b1058a81c935cd0fb006ab0e732ef4aae2cce5b
Brianna Keune <briannakeune@gmail.com>
2020-01-08 14:08:44 -0600

    added validation of body requirements, recipeId

--- api/routes/recipe_ingredients/middleware/validateAddIngredientRequirements.js
+++ api/routes/recipe_ingredients/middleware/validateAddIngredientRequirements.js
@@ -1,5 +1,29 @@
-const {} = require("");
-
 module.exports = function validateAddIngredientRequirements(req, res, next) {
-    return null
-}
+  const required = {
+    recipe_id: req.params.recipe_id,
+    ingredient_name: req.body.ingredient_name,
+    quantity_id: req.body.quantity_id,
+    quantity_value: req.body.quantity_value
+  };
+
+  switch (required) {
+    case !require.recipe_id:
+      return res
+        .status(400)
+        .json({ message: "A recipe_id is required as part of the body." });
+    case !require.ingredient_name:
+      return res.status(400).json({
+        message: "A ingredient_name is required as part of the body."
+      });
+    case !require.quantity_id:
+      return res
+        .status(400)
+        .json({ message: "A quantity_id is required as part of the body." });
+    case !require.quantity_value:
+      return res
+        .status(400)
+        .json({ message: "A quantity_value is required as part of the body." });
+    default:
+      return next();
+  }
+};
--- api/routes/recipe_ingredients/middleware/validateRecipeId.js
+++ api/routes/recipe_ingredients/middleware/validateRecipeId.js
@@ -1,5 +1,20 @@
-const {} = require("");
+const { getById } = require("../../recipes/model.js");
 
 module.exports = function validateRecipeId(req, res, next) {
-    return null
-}
+  const { recipe_id } = req.params;
+
+  getById({ id: recipe_id })
+    .then(recipe => {
+      if (!recipe.length)
+        res.status(409).json({
+          message: `Recipe with the id: ${recipe_id} could not be found`
+        });
+      else next();
+    })
+    .catch(err =>
+      res.status(500).json({
+        error: `Error validating recipe with the id: ${recipe_id}.`,
+        err
+      })
+    );
+};
