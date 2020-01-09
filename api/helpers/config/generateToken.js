0ad40773ffa1cd8202bb9264be85c88276dc453b
Brianna Keune <briannakeune@gmail.com>
2020-01-09 13:41:27 -0600

    adding webtoken stuff

--- api/helpers/config/generateToken.js
+++ api/helpers/config/generateToken.js
@@ -8,11 +8,11 @@ module.exports = {
 function generateToken({ id, username }) {
   // config for payload part of token
   const payload = {
-    subject: id,
-    username: username
+    id: id,
+    username: username,
   };
   const options = {
-    expiresIn: "2h"
+    expiresIn: "8h"
   };
 
   // signiture of token
--- api/routes/chef_recipes/router.js
+++ api/routes/chef_recipes/router.js
@@ -65,7 +65,7 @@ router.post(
           };
           res.status(201).json(response);
         })
-        .catch(err => res.status(500).json({ error: err.message }));
+        .catch(err => res.status(500).json({ message:'Error in ',error: err.message }));
     });
   }
 );
