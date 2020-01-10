eeecd26de6ff7fa8de1fe9642452644361cb257c
Brianna Keune <briannakeune@gmail.com>
2020-01-10 01:04:20 -0600

    need to figure out why testing works sometimes but not all the time

--- api/server.test.js
+++ api/server.test.js
@@ -2,8 +2,8 @@ const request = require("supertest");
 const server = require("./server.js");
 
 describe("GET /", () => {
-  it('has process.env.NODE_ENV as "testing"', () => {
-    expect(process.env.NODE_ENV).toBe("testing");
+  it('has process.env.DB_ENV as "testing"', () => {
+    expect(process.env.DB_ENV).toBe("testing");
   });
   it("returns 200 ok", done => {
     return request(server)
